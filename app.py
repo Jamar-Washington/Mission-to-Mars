43e0fa8303bf5e790b9e63f5aa9a44ddc39efdc3
Jamar-Washington <jamarxwashington@gmail.com>
2022-10-18 14:09:08 -0500

    beginning to add mongo to web app

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask_pymongo import PyMongo
 import scraping
 
 # Set up flask
-app = Flask(__name__)
+app = Flask(__name__, template_folder='.')
 
 # Use flask_pymongo to set up mongo connection
 app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
@@ -23,6 +23,6 @@ def scrape():
    mars_data = scraping.scrape_all()
    mars.update_one({}, {"$set":mars_data}, upsert=True)
    return redirect('/', code=302)
-   
+
 if __name__ == "__main__":
    app.run()
